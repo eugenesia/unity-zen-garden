d82024d0f297cb8ea490e0c497d67c1f641580ca
Eugene Sia <eugene@eugenesia.co.uk>
2016-11-14 01:39:01 +0000

    Add player movement script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Control player movement.
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+
+	void Start () {
+		// Get the RigidBody component so we can modify it later.
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Apply forces to RigidBody every fixed frame update.
+	// E.g. every physics step.
+	void FixedUpdate () {
+
+		// Detect keypresses.
+		float moveHor = Input.GetAxis("Horizontal");
+		float moveVert = Input.GetAxis("Vertical");
+
+		// Convert keypresses to movement on the ground.
+		Vector3 movement = new Vector3 (moveHor, 0.0f, moveVert);
+
+		rb.AddForce(movement * speed);
+	}
+}
