f9be44263daa01446029ab5d532881ce546787c9
Eugene Sia <eugene@eugenesia.co.uk>
2016-11-24 05:26:15 +0000

    Rotate FloorCanvas with MainCamera

---
+++ Assets/Scripts/RotateWithMainCamera.cs
@@ -0,0 +1,27 @@
+﻿///
+/// Rotate the object horizontally, with the main camera.
+///
+
+using UnityEngine;
+using System.Collections;
+
+public class RotateWithMainCamera : MonoBehaviour {
+
+	private GameObject mainCam;
+
+	// Use this for initialization
+	void Start () {
+		mainCam = Camera.main.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Get horizontal component of camera's orientation, by projecting
+		// the vector onto the horizontal plane.
+		Vector3 camForwardHoriz = Vector3.ProjectOnPlane(
+			mainCam.transform.forward, Vector3.up);
+
+		// Rotate this object horizontally, to align with cam. 
+		transform.rotation = Quaternion.LookRotation(camForwardHoriz);
+	}
+}
