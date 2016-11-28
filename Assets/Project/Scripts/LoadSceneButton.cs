1c119b982dfaa7e3cbfe6001c3c0e80ed22e2a49
Eugene Sia <eugene@eugenesia.co.uk>
2016-11-28 04:59:34 +0000

    Trigger button click on gaze timer expired

--- Assets/Project/Scripts/LoadSceneButton.cs
+++ Assets/Project/Scripts/LoadSceneButton.cs
@@ -5,6 +5,7 @@
 /// Ref: https://www.youtube.com/watch?v=M6sL0ffosds
 /// </summary>
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class LoadSceneButton : MonoBehaviour {
@@ -16,15 +17,20 @@ public class LoadSceneButton : MonoBehaviour {
 	private bool isLookedAt = false;
 
 	// How long the user can gaze at this before the button is clicked.
-	public float timerDuration = 2f;
+	public float timerDuration = 3f;
 
 	// Count time the player has been gazing at the button.
 	private float lookTimer = 0f;
 
+	// Gaze timer indicator.
+	private GameObject gazeTimer;
+
 
 	// Use this for initialization
 	void Start () {
-		sceneController = GameObject.Find("SceneController");	
+		sceneController = GameObject.Find("SceneController");
+
+		gazeTimer = GameObject.Find("GazeTimer");
 	}
 	
 	// Update is called once per frame
@@ -36,12 +42,23 @@ public class LoadSceneButton : MonoBehaviour {
 			// Increment the gaze timer.
 			lookTimer += Time.deltaTime;
 
+			// Set cutoff value on gaze timer indicator so it shows a part circle
+			// indicating time left.
+			gazeTimer.GetComponent<Renderer>().material.SetFloat("_Cutoff", 1f - lookTimer / timerDuration);
+
 			// Gaze time exceeded limit - button is considered clicked.
 			if (lookTimer > timerDuration) {
 				lookTimer = 0f;
 
 				Debug.Log("Button selected!");
+				GetComponent<Button>().onClick.Invoke();
 			}
+		}
+
+		// Not gazing at this anymore, reset everything.
+		else {
+			lookTimer = 0f;
+			gazeTimer.GetComponent<Renderer>().material.SetFloat("_Cutoff", 1f);
 		}
 	}
 
